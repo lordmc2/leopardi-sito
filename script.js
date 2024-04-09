21911806a64d840d815ff0db475adc2b161d2e69
fraanctttwee <163666436+fraanctttwee@users.noreply.github.com>
2024-04-09 20:11:58 +0200

    sito-leopardi

---
+++ script.js
@@ -0,0 +1,8 @@
+// Eventuale codice JavaScript per migliorare l'interattività del sito
+const backgroundImageUrl = 'leopardi.jpg'; // Assicurati di sostituire con il percorso corretto dell'immagine
+
+  // Aggiungi l'immagine di sfondo al container
+  const container = document.getElementById('container');
+  container.style.backgroundImage = `url('${backgroundImageUrl}')`;
+  container.style.backgroundSize = 'cover';
+  container.style.backgroundPosition = 'center';
